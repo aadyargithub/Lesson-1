73b117109f681692014b20bebdae8ae7fd03e8a1
aadyargithub <aadya.ravipati@gmail.com>
2025-01-19 12:50:09 -0800

    the code is not working. would you mind taking a look?

---
+++ swapping.py
@@ -0,0 +1,13 @@
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
+print("number1: ", num1)
+print("number2: ", num2)
+print("number3: ", num3)
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+print("After Swapping")
+print("number1: ", num1)
+print("number2: ", num2)
